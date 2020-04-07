02e1e9ede76cb156ad5f55ab96d1953b44f9077c
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-04-07 15:36:40 -0400

    :construction: Development path.

--- tests/test_mopi.m
+++ tests/test_mopi.m
@@ -103,9 +103,9 @@ end
 
 function test_shellscript_error_noinput()
     if ispc
-        cmd = 'bash ./mopi.sh';
+        cmd = 'bash ../mopi.sh';
     else        
-        cmd = './mopi.sh';
+        cmd = '../mopi.sh';
     end
     status = system(cmd);
     assertTrue(status~=0);
@@ -192,11 +192,11 @@ function check_url(method, extension, addInlineComment)
             if ispc
                 FNAME_TO_USE = convertPcToUnixPath(FNAME);
                 PKG_DIR_TO_USE = convertPcToUnixPath(PKG_DIR);
-                cmd = 'bash ./mopi.sh';
+                cmd = 'bash ../mopi.sh';
             else
                 FNAME_TO_USE = FNAME;
                 PKG_DIR_TO_USE = PKG_DIR;
-                cmd = './mopi.sh';
+                cmd = '../mopi.sh';
             end
             [status, result] = system(sprintf([cmd ' %s %s -'], FNAME_TO_USE, PKG_DIR_TO_USE));
             assertEqual(0, status, result);
@@ -247,11 +247,11 @@ function check_fex(method, includeProtocol)
             if ispc
                 FNAME_TO_USE = convertPcToUnixPath(FNAME);
                 PKG_DIR_TO_USE = convertPcToUnixPath(PKG_DIR);
-                cmd = 'bash ./mopi.sh';
+                cmd = 'bash ../mopi.sh';
             else
                 FNAME_TO_USE = FNAME;
                 PKG_DIR_TO_USE = PKG_DIR;
-                cmd = './mopi.sh';
+                cmd = '../mopi.sh';
             end
             [status, result] = system(sprintf([cmd ' %s %s -'], FNAME_TO_USE, PKG_DIR_TO_USE));
             assertEqual(0, status, result);
@@ -327,11 +327,11 @@ function check_full(method)
             if ispc
                 FNAME_TO_USE = convertPcToUnixPath(FNAME);
                 PKG_DIR_TO_USE = convertPcToUnixPath(PKG_DIR);
-                cmd = 'bash ./mopi.sh';
+                cmd = 'bash ../mopi.sh';
             else
                 FNAME_TO_USE = FNAME;
                 PKG_DIR_TO_USE = PKG_DIR;
-                cmd = './mopi.sh';
+                cmd = '../mopi.sh';
             end
             [status, result] = system(sprintf([cmd ' %s %s -'], FNAME_TO_USE, PKG_DIR_TO_USE));
             assertEqual(0, status, result);
@@ -372,10 +372,10 @@ function check_shellscript_error(entry)
     fclose(fid);
     if ispc
         FNAME_TO_USE = convertPcToUnixPath(FNAME);
-        cmd = 'bash ./mopi.sh';
+        cmd = 'bash ../mopi.sh';
     else
         FNAME_TO_USE = FNAME;
-        cmd = './mopi.sh';
+        cmd = '../mopi.sh';
     end
     [status, result] = system(sprintf([cmd ' %s'], FNAME_TO_USE));
     assertTrue(status~=0, result);
@@ -469,11 +469,11 @@ function check_forge(method, includeProtocol)
             if ispc
                 FNAME_TO_USE = convertPcToUnixPath(FNAME);
                 PKG_DIR_TO_USE = convertPcToUnixPath(PKG_DIR);
-                cmd = 'bash ./mopi.sh';
+                cmd = 'bash ../mopi.sh';
             else
                 FNAME_TO_USE = FNAME;
                 PKG_DIR_TO_USE = PKG_DIR;
-                cmd = './mopi.sh';
+                cmd = '../mopi.sh';
             end
             [status, result] = system(sprintf([cmd ' %s %s -'], FNAME_TO_USE, PKG_DIR_TO_USE));
             assertEqual(0, status, result);
